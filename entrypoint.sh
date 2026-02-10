dfa3d7afb7df85ed98a8a6972e7645586dbe72d4
binaya.parajuli <binaya.parajuli@nicnepal.org>
2026-02-10 21:07:34 +0545

    entrypont

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ echo "Running migrations..."
 python manage.py migrate
 
 echo "Creating superuser (if not exists)..."
+
 python manage.py create_admin || true
 python manage.py import_csv || true
 python manage.py load_surname_mapping || true
