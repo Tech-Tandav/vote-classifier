a03fe7f20ff7887d36ccfbcccfd33bc1567a137e
binaya.parajuli <binaya.parajuli@nicnepal.org>
2026-02-10 20:40:16 +0545

    fix

--- backend/settings.py
+++ backend/settings.py
@@ -164,17 +164,14 @@ SPECTACULAR_SETTINGS = {
 
 
 # CORS Settings (for frontend integration)
-CORS_ALLOWED_ORIGINS = os.environ.get(
-    'CORS_ALLOWED_ORIGINS',
-    "http://localhost:3000",
-    "http://202.79.51.253:9494"
-).split(',')
+CORS_ALLOWED_ORIGINS = ["http://localhost:8000","http://localhost:3000",,"http://202.79.51.253:9494"]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
 # CSRF Settings
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:8000",
     "http://127.0.0.1:3000",
     "http://202.79.51.253:9494"
 ]
