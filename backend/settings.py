31e38b7c1010eb8f6aa945c477a7a526567ae7ee
SGsantos55 <santosgadtaula@gmail.com>
2026-02-18 01:43:22 +0545

    fixed bug

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key-change-in-pro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ["202.79.51.253", "localhost", "127.0.0.1", "api.voter.election.dahalpradeep.com.np"]
+ALLOWED_HOSTS = ["202.79.51.253", "localhost", "127.0.0.1", "api.voter.election.dahalpradeep.com.np", "django"]
 
 # Application definition
 INSTALLED_APPS = [
