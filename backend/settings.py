e8f12f6fd04df03a950fefc4683968c5222da544
binaya.parajuli <binaya.parajuli@nicnepal.org>
2026-02-19 11:50:51 +0545

    fix

--- backend/settings.py
+++ backend/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'election' / 'db.sqlite3',
+        'NAME': BASE_DIR / 'election' / 'election.sqlite3',
         "OPTIONS": {
             "timeout": 8000,  # seconds
         },
